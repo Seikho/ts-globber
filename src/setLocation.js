4b8744e1f5d497c670b4f1a5288a6c851fe05904
Carl Winkler <carl@carlossus.com>
2015-08-24 16:58:22 +0800

    Delete redundant modules

--- src/setLocation.js
+++
@@ -1,7 +0,0 @@
-var path = require("path");
-function setLocation(tsconfigLocation) {
-    // For use as an external module
-    this.tsconfigLocation = path.resolve(tsconfigLocation || "tsconfig.json");
-    return this.tsconfigLocation.indexOf("tsconfig.json") >= 0;
-}
-module.exports = setLocation;
--- src/setLocation.ts
+++
@@ -1,8 +0,0 @@
-import path = require("path");
-export = setLocation;
-
-function setLocation(tsconfigLocation?: string) {
-	// For use as an external module
-	this.tsconfigLocation = path.resolve(tsconfigLocation || "tsconfig.json");
-	return this.tsconfigLocation.indexOf("tsconfig.json") >= 0;
-}
