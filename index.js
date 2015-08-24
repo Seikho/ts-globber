92045b65fadd4cd68015d3fefd5d05d50857782c
Carl Winkler <carl@carlossus.com>
2015-08-24 16:59:23 +0800

    Move all code to single module and implement recursion handling

--- index.js
+++ index.js
@@ -1,16 +1,90 @@
+var glob = require("glob");
 var fs = require("fs");
-var setLocation = require("./src/setLocation");
-var readConfig = require("./src/readConfig");
+var path = require("path");
+var isRecursive = process.argv[2] === "-r";
 function updateTsconfig(tsconfigFile) {
-    this.tsconfigLocation = "";
-    this.files = [];
-    this.returnCount = 0;
-    this.globCount = 0;
-    this.tsconfig = "";
-    if (!setLocation(tsconfigFile || null)) {
+    var location = setLocation(tsconfigFile || null);
+    if (!location) {
         console.log("Failed to update tsconfig.json: Could not set location");
         return;
     }
-    fs.readFile(this.tsconfigLocation, "utf-8", readConfig);
+    readConfig(location);
+}
+function readConfig(location) {
+    fs.readFile(location, "utf-8", function (error, file) {
+        if (error)
+            return;
+        var tsconfig;
+        try {
+            tsconfig = JSON.parse(file);
+        }
+        catch (ex) {
+            console.log("Failed to parse tsconfig.config as JSON");
+            console.log(ex.message.toString());
+            return;
+        }
+        var tsGlob = tsconfig["filesGlob"];
+        // If there isn't a filesGlob property, don't continue.
+        if (!tsGlob)
+            return;
+        var globCount = tsGlob.length;
+        var tsObj = {
+            config: tsconfig,
+            location: location,
+            count: globCount,
+            files: [],
+            returnCount: 0
+        };
+        var split = location.split(path.sep);
+        var globModule = isRecursive
+            ? split[split.length - 2] + "/"
+            : "";
+        tsObj.module = globModule;
+        for (var g in tsGlob) {
+            var globLocation = path.join(globModule, tsGlob[g]);
+            console.log(process.cwd());
+            console.log(globLocation);
+            console.log("------");
+            glob(globLocation, function (err, matches) { return parseGlob(err, matches, tsObj); });
+        }
+    });
+}
+function tryParse(file) {
+    try {
+        return JSON.parse(file.trim());
+    }
+    catch (ex) {
+        console.log("Failed to parse tsconfig.json as JSON");
+        console.log(ex.message.toString());
+        throw new Error("Failed to parse tsconfig.json as JSON");
+    }
+}
+function parseGlob(err, matches, tsObj) {
+    if (isRecursive) {
+        matches = matches.map(function (m) { return m.replace(tsObj.module, ""); });
+    }
+    // Add the 'matches' to our file list
+    tsObj.files = tsObj.files.concat(matches);
+    // Once we have all of our results, apply the changes.
+    if (++tsObj.returnCount === tsObj.count)
+        applyChanges(tsObj);
+}
+function applyChanges(tsObj) {
+    tsObj.config["files"] = tsObj.files;
+    // Compose the new tsconfig.json file
+    var newConfig = JSON.stringify(tsObj.config, null, 4);
+    // Replace the existing tsconfig.json file
+    fs.writeFile(tsObj.location, newConfig, function (err) {
+        if (err)
+            console.log("Failed to update " + tsObj.location);
+        else
+            console.log("Successfully updated " + tsObj.location);
+    });
+}
+function setLocation(tsconfigLocation) {
+    var location = path.resolve(tsconfigLocation || "tsconfig.json");
+    return location.indexOf("tsconfig.json") >= 0
+        ? location
+        : null;
 }
 module.exports = updateTsconfig;
--- index.ts
+++ index.ts
@@ -1,21 +1,111 @@
 import glob = require("glob");
 import fs = require("fs");
 import path = require("path");
-import setLocation = require("./src/setLocation");
-import readConfig = require("./src/readConfig");
 export = updateTsconfig;
 
+var isRecursive = process.argv[2] === "-r";
 function updateTsconfig(tsconfigFile?: string) {
-	this.tsconfigLocation = "";
-	this.files = [];
-	this.returnCount = 0;
-	this.globCount = 0;
- 	this.tsconfig = "";
+
+    var location = setLocation(tsconfigFile || null);
+    if (!location) {
+        console.log("Failed to update tsconfig.json: Could not set location");
+        return;
+    }
+
+    readConfig(location);
+}
+
+function readConfig(location: string) {
+    fs.readFile(location, "utf-8", (error, file) => {
+        if (error) return;
+        var tsconfig;
+
+        try {
+            tsconfig = JSON.parse(file);
+        } catch (ex) {
+            console.log("Failed to parse tsconfig.config as JSON");
+            console.log(ex.message.toString());
+            return;
+        }
+
+        var tsGlob: string[] = tsconfig["filesGlob"];
 	
-	if (!setLocation(tsconfigFile || null)) {
-		console.log("Failed to update tsconfig.json: Could not set location");
-		return;
-	}
+        // If there isn't a filesGlob property, don't continue.
+        if (!tsGlob) return;
+
+        var globCount = tsGlob.length;
+
+        var tsObj: TsObject = {
+            config: tsconfig,
+            location,
+            count: globCount,
+            files: [],
+            returnCount: 0
+        }
+
+        var split = location.split(path.sep);
+        var globModule = isRecursive
+            ? split[split.length - 2] + "/"
+            : "";
+            
+        tsObj.module = globModule;
+
+        for (var g in tsGlob) {
+            var globLocation = path.join(globModule, tsGlob[g]);
+            glob(globLocation, (err, matches) => parseGlob(err, matches, tsObj));
+        }
+    });
+
+}
+
+function tryParse(file: string) {
+    try {
+        return JSON.parse(file.trim());
+    } catch (ex) {
+        console.log("Failed to parse tsconfig.json as JSON");
+        console.log(ex.message.toString());
+        throw new Error("Failed to parse tsconfig.json as JSON");
+    }
+}
+
+function parseGlob(err, matches: string[], tsObj: TsObject) {
+    if (isRecursive) {
+        matches = matches.map(m => m.replace(tsObj.module,""))
+    }
+        
+    // Add the 'matches' to our file list
+    tsObj.files = tsObj.files.concat(matches);
 	
-	fs.readFile(this.tsconfigLocation, "utf-8", readConfig);
+    // Once we have all of our results, apply the changes.
+    if (++tsObj.returnCount === tsObj.count) applyChanges(tsObj);
+}
+
+function applyChanges(tsObj: TsObject) {
+    tsObj.config["files"] = tsObj.files;
+	
+    // Compose the new tsconfig.json file
+    var newConfig = JSON.stringify(tsObj.config, null, 4);
+	
+    // Replace the existing tsconfig.json file
+    fs.writeFile(tsObj.location, newConfig, (err) => {
+        if (err) console.log("Failed to update " + tsObj.location);
+        else console.log("Successfully updated " + tsObj.location);
+    });
+}
+
+function setLocation(tsconfigLocation?: string) {
+
+    var location = path.resolve(tsconfigLocation || "tsconfig.json");
+    return location.indexOf("tsconfig.json") >= 0
+        ? location
+        : null;
+}
+
+interface TsObject {
+    module?: string;
+    config: any;
+    location: string;
+    count: number;
+    files: string[];
+    returnCount: number;
 }
