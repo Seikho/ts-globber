fa77a0601d5fb178b2f5a631e80a170c2b285a50
Carl Winkler <carl@carlossus.com>
2015-08-24 16:59:01 +0800

    Remove unit tests

--- test/index.js
+++ test/index.js
@@ -1,12 +1,12 @@
-var setLocation = require("../src/setLocation");
-var chai = require("chai");
-var expect = chai.expect;
-var tsconfigLocation = "";
-describe("location tests", function () {
-    it("will fail to parse if 'tsconfig.json' is not in the string", function () {
-        expect(setLocation("empty")).to.equal(false);
-    });
-    it("will successfully parse if 'tsconfig.json' is in the string", function () {
-        expect(setLocation("tsconfig.json")).to.equal(true);
-    });
-});
+// import setLocation = require("../src/setLocation");
+// import chai = require("chai");
+// var expect = chai.expect;
+// var tsconfigLocation = "";
+// describe("location tests", () => {
+// 	it("will fail to parse if 'tsconfig.json' is not in the string", () => {
+// 		expect(setLocation("empty")).to.equal(false);
+// 	});
+// 	it ("will successfully parse if 'tsconfig.json' is in the string", () => {
+// 		expect(setLocation("tsconfig.json")).to.equal(true);
+// 	});
+// }); 
--- test/index.ts
+++ test/index.ts
@@ -1,14 +1,14 @@
-import setLocation = require("../src/setLocation");
-import chai = require("chai");
-var expect = chai.expect;
-var tsconfigLocation = "";
+// import setLocation = require("../src/setLocation");
+// import chai = require("chai");
+// var expect = chai.expect;
+// var tsconfigLocation = "";
 
-describe("location tests", () => {
-	it("will fail to parse if 'tsconfig.json' is not in the string", () => {
-		expect(setLocation("empty")).to.equal(false);
-	});
+// describe("location tests", () => {
+// 	it("will fail to parse if 'tsconfig.json' is not in the string", () => {
+// 		expect(setLocation("empty")).to.equal(false);
+// 	});
 	
-	it ("will successfully parse if 'tsconfig.json' is in the string", () => {
-		expect(setLocation("tsconfig.json")).to.equal(true);
-	});
-});
+// 	it ("will successfully parse if 'tsconfig.json' is in the string", () => {
+// 		expect(setLocation("tsconfig.json")).to.equal(true);
+// 	});
+// });
