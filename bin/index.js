6e995891503d20e3c82b74fe07021b7bb290e35f
Carl Winkler <carl@carlossus.com>
2015-08-24 16:58:38 +0800

    Add recursive option to CLI

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
 var update = require("../index");
+var recurse = require("../recurse");
+
 if (process.argv.length > 2) {
 	var args = process.argv.slice(2);
-	update(args.join(" "));
+
+    if (args[0] === "-r") recurse(process.argv[3] || process.env.PWD);
+	else update(args.join(" "));
 }
 else update();
